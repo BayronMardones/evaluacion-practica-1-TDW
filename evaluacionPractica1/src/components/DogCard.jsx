763fcb09c6aee9c4a87864b61d52c24866df00bf
rodripgg <rodrigo.parra1901@alumnos.ubiobio.cl>
2023-10-02 23:09:33 -0300

    lista de aceptado y rechazado a medio hacer

--- evaluacionPractica1/src/components/DogCard.jsx
+++ evaluacionPractica1/src/components/DogCard.jsx
@@ -5,21 +5,35 @@ const DogCard = () => {
   const [dogImageUrl, setDogImageUrl] = useState('');
   // Estado para el nombre del perro candidato (generado aleatoriamente)
   const [dogName, setDogName] = useState('');
+  const [acceptedDogs, setAcceptedDogs] = useState([]);
+  const [rejectedDogs, setRejectedDogs] = useState([]);
+
 
   // Función para cargar una nueva imagen de perro al azar
-  const fetchRandomDog = async () => {
-    // Coloca aquí la lógica para hacer la solicitud GET a la API de perros
+  const fetchRandomDog = async (accept) => {
     try {
       const response = await fetch('https://dog.ceo/api/breeds/image/random');
       const data = await response.json();
       const imageUrl = data.message;
-      setDogImageUrl(imageUrl);
-      // Generar un nuevo nombre de perro cada vez que se obtiene una imagen nueva
+  
+      // Genera un nuevo nombre de perro
       generateRandomName();
+  
+      // Actualiza la imagen del perro
+      setDogImageUrl(imageUrl);
+  
+      // Si se acepta el perro, agrega el nombre a la lista de perros aceptados
+      if (accept) {
+        setAcceptedDogs([...acceptedDogs, dogName]);
+      } else if(accept == false){
+        // Si se rechaza el perro, agrega el nombre a la lista de perros rechazados
+        setRejectedDogs([...rejectedDogs, dogName]);
+      }
     } catch (error) {
       console.error('Error al cargar la imagen del perro:', error);
     }
   };
+  
 
   // Función para generar un nombre de perro aleatorio (6 caracteres)
   const generateRandomName = () => {
@@ -38,16 +52,38 @@ const DogCard = () => {
   }, []); // El array vacío asegura que esto se ejecute solo una vez al cargar el componente
 
   return (
+    <body>
     <div className="dog-card">
       <img
         src={dogImageUrl}
         alt="Perro candidato"
-        style={{ width: '500px', height: '500px' }} 
+        style={{ width: '400px', height: '400px' }} 
       />
       <h2>Nombre: {dogName}</h2>
-      <button onClick={fetchRandomDog}>Aceptar</button>
-      <button onClick={fetchRandomDog}>Rechazar</button>
+      <button onClick={() => fetchRandomDog(true)}>Aceptar</button>
+      <button onClick={() => fetchRandomDog(false)}>Rechazar</button>
     </div>
+
+    <body className="dog-lists">
+      <div className="accepted-list">
+      <h3>Perros Aceptados</h3>
+      <ul>
+        {acceptedDogs.map((dog, index) => (
+          <li key={index}>{dog}</li>
+        ))}
+      </ul>
+      </div>
+    <div className="rejected-list">
+      <h3>Perros Rechazados</h3>
+      <ul>
+        {rejectedDogs.map((dog, index) => (
+          <li key={index}>{dog}</li>
+        ))}
+      </ul>
+    </div>
+    </body>
+    
+  </body>
   );
 };
 
