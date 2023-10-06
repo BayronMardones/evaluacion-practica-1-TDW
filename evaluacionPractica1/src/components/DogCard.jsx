e0f26fd2a25bbf2ff930ce6fb9cf911a467fd5d8
BayronMardones <bayron.mardones1901@alumnos.ubiobio.cl>
2023-10-06 19:09:18 -0300

    descripcion aleatoria perro

--- evaluacionPractica1/src/components/DogCard.jsx
+++ evaluacionPractica1/src/components/DogCard.jsx
@@ -11,6 +11,7 @@ const DogCard = () => {
 
   // Función para cargar una nueva imagen de perro al azar
   const fetchRandomDog = async (accept) => {
+    console.log("hola")
     try {
       const response = await fetch("https://dog.ceo/api/breeds/image/random");
       const data = await response.json();
@@ -28,6 +29,7 @@ const DogCard = () => {
       } else if (accept == false) {
         // Si se rechaza el perro, agrega el nombre a la lista de perros rechazados
         setRejectedDogs([...rejectedDogs, dogName]);
+        console.log(dogInfo);
       }
     } catch (error) {
       console.error("Error al cargar la imagen del perro:", error);
@@ -45,59 +47,71 @@ const DogCard = () => {
     setDogName(randomName);
   };
 
+  //generar descripcion aleatoria a cada perro
+
+  const generateRandomDescription = () => {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let randomDescription = '';
+    for (let i = 0; i < 20; i++) {
+      const randomIndex = Math.floor(Math.random() * alphabet.length);
+      randomDescription += alphabet[randomIndex];
+    }
+    setDogDescription(randomDescription);
+  };
+
   // Efecto para cargar una imagen y generar un nombre al cargar el componente
   useEffect(() => {
     fetchRandomDog();
   }, []); // El array vacío asegura que esto se ejecute solo una vez al cargar el componente
 
   return (
-      <Grid container spacing={4}>
-        <Grid item xs={6} className="dog-card">
-          <img
-            src={dogImageUrl}
-            alt="Perro candidato"
-            style={{ width: "400px", height: "400px" }}
-          />
-          <h2>Nombre: {dogName}</h2>
-          <div className="button-container">
-            <button
-              className="accept-button"
-              onClick={() => fetchRandomDog(true)}
-            >
-              Aceptar
-            </button>
-            <button
-              className="reject-button"
-              onClick={() => fetchRandomDog(false)}
-            >
-              Rechazar
-            </button>
-          </div>
-        </Grid>
-
-        <Grid item xs className="accepted-list">
-          <div>
-            <h3>Perros Aceptados</h3>
-            <ul>
-              {acceptedDogs.map((dog, index) => (
-                <li key={index}>{dog}</li>
-              ))}
-            </ul>
-          </div>
-        </Grid>
-
-        <Grid item xs className="rejected-list">
-          <div>
-            <h3>Perros Rechazados</h3>
-            <ul>
-              {rejectedDogs.map((dog, index) => (
-                <li key={index}>{dog}</li>
-              ))}
-            </ul>
-          </div>
-        </Grid>
+    <Grid container spacing={4}>
+      <Grid item xs={6} className="dog-card">
+        <img
+          src={dogImageUrl}
+          alt="Perro candidato"
+          style={{ width: "400px", height: "400px" }}
+        />
+        <h2>Nombre: {dogName}</h2>
+        <div className="button-container">
+          <button
+            className="accept-button"
+            onClick={() => fetchRandomDog(true)}
+          >
+            Aceptar
+          </button>
+          <button
+            className="reject-button"
+            onClick={() => fetchRandomDog(false)}
+          >
+            Rechazar
+          </button>
+        </div>
       </Grid>
-    
+
+      <Grid item xs className="accepted-list">
+        <div>
+          <h3>Perros Aceptados</h3>
+          <ul>
+            {acceptedDogs.map((dog, index) => (
+              <li key={index}>{dog}</li>
+            ))}
+          </ul>
+        </div>
+      </Grid>
+
+      <Grid item xs className="rejected-list">
+        <div>
+          <h3>Perros Rechazados</h3>
+          <ul>
+            {rejectedDogs.map((dog, index) => (
+              <li key={index}>{dog}</li>
+            ))}
+          </ul>
+        </div>
+      </Grid>
+    </Grid>
+
   );
 };
 
