bba7e91d2fbf46a93323935243c4ca2d242515c5
BayronMardones <bayron.mardones1901@alumnos.ubiobio.cl>
2023-10-06 19:09:18 -0300

    descripcion aleatoria perro

--- evaluacionPractica1/src/components/DogCard.jsx
+++ evaluacionPractica1/src/components/DogCard.jsx
@@ -7,10 +7,12 @@ const DogCard = () => {
   const [dogName, setDogName] = useState('');
   const [acceptedDogs, setAcceptedDogs] = useState([]);
   const [rejectedDogs, setRejectedDogs] = useState([]);
-
+  const [dogDescription, setDogDescription] = useState('');
+ 
 
   // Función para cargar una nueva imagen de perro al azar
   const fetchRandomDog = async (accept) => {
+    console.log("hola")
     try {
       const response = await fetch('https://dog.ceo/api/breeds/image/random');
       const data = await response.json();
@@ -18,16 +20,24 @@ const DogCard = () => {
   
       // Genera un nuevo nombre de perro
       generateRandomName();
+
+      //genera una nueva descripcion de perro
+      generateRandomDescription();
   
       // Actualiza la imagen del perro
       setDogImageUrl(imageUrl);
   
       // Si se acepta el perro, agrega el nombre a la lista de perros aceptados
       if (accept) {
-        setAcceptedDogs([...acceptedDogs, dogName]);
+        const dogInfo = {
+          name: dogName,
+          description: dogDescription
+        };
+        setAcceptedDogs([...acceptedDogs, dogInfo]);
       } else if(accept == false){
         // Si se rechaza el perro, agrega el nombre a la lista de perros rechazados
         setRejectedDogs([...rejectedDogs, dogName]);
+        console.log(dogInfo);
       }
     } catch (error) {
       console.error('Error al cargar la imagen del perro:', error);
@@ -46,6 +56,18 @@ const DogCard = () => {
     setDogName(randomName);
   };
 
+   //generar descripcion aleatoria a cada perro
+
+   const generateRandomDescription = () => {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let randomDescription = '';
+    for (let i = 0; i < 20; i++) {
+      const randomIndex = Math.floor(Math.random() * alphabet.length);
+      randomDescription += alphabet[randomIndex];
+    }
+    setDogDescription(randomDescription);
+  };
+
   // Efecto para cargar una imagen y generar un nombre al cargar el componente
   useEffect(() => {
     fetchRandomDog();
@@ -60,6 +82,7 @@ const DogCard = () => {
         style={{ width: '400px', height: '400px' }} 
       />
       <h2>Nombre: {dogName}</h2>
+      <h2>Descripcion: {dogDescription}</h2>
       <button onClick={() => fetchRandomDog(true)}>Aceptar</button>
       <button onClick={() => fetchRandomDog(false)}>Rechazar</button>
     </div>
@@ -69,7 +92,9 @@ const DogCard = () => {
       <h3>Perros Aceptados</h3>
       <ul>
         {acceptedDogs.map((dog, index) => (
-          <li key={index}>{dog}</li>
+          <li key={index}>{dog.description}</li> 
+
+
         ))}
       </ul>
       </div>
@@ -77,7 +102,12 @@ const DogCard = () => {
       <h3>Perros Rechazados</h3>
       <ul>
         {rejectedDogs.map((dog, index) => (
-          <li key={index}>{dog}</li>
+          <li key={index}>{dog}</li> 
+        //   <li key={index}>
+        // <img src={dog.imageURL} alt={dog.name} />
+        // {dog.name}
+      // </li>
+           
         ))}
       </ul>
     </div>
